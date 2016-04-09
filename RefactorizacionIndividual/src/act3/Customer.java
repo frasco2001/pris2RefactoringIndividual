b8151e17c1170cfe8eed5e327f2c394779bfb942
frasco2001 <frasco2001@172.16.60.1>
2016-04-09 21:17:06 +0200

    //3 El metodo amountFor, no utiliza información del cliente, sino del alquiler, por lo que dicho metodo debería pertenecer a la clase Rental y no a Customer, es por ello por lo que la movemos de clase, perdiendo el la nueva el parametro.

--- RefactorizacionIndividual/src/act3/Customer.java
+++ RefactorizacionIndividual/src/act3/Customer.java
@@ -28,8 +28,8 @@ class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			
-			//1
-			thisAmount = amountFor(each);
+			//3
+			thisAmount = each.getCharge();
 			
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -44,30 +44,5 @@ class Customer {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
-	}
-
-	/**
-	 * @param aRental
-	 * @return
-	 */
-	//2
-	private double amountFor(Rental aRental) {
-		double thisAmount = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 }
