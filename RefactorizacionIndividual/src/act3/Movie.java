d1b453e9e9b9ad419abcc5af40dffc1a95818799
frasco2001 <frasco2001@172.16.60.1>
2016-04-09 23:30:52 +0200

    //9 Vamos a mover el metodo que contiene el switch, para ubicarlo en la clase donde hace referencia a sus datos, en este caso Movie, insertando en este caso un parametro nuevo, con los dias del alquiler.

--- RefactorizacionIndividual/src/act3/Movie.java
+++ RefactorizacionIndividual/src/act3/Movie.java
@@ -1,4 +1,5 @@
 package act3;
+
 public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
@@ -21,5 +22,26 @@ public class Movie {
 
 	public String getTitle() {
 		return _title;
+	}
+
+	// 9
+	public double getCharge(int daysRented) {
+		double result = 0;
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2)
+				result += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (daysRented > 3)
+				result += (daysRented - 3) * 1.5;
+			break;
+		}
+		return result;
 	}
 }
